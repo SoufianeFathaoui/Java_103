dead5823daf1373c23da680b48318f43c7e06d87
Soufiane Fathaoui <soufianenakata7@gmail.com>
2024-03-15 21:16:32 +0000

    New editing...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        A a = new A();
+        B b = new B();
+        Test test = new Test();
+        test.printValue(b);
+        // This is a anonymouse object and class, that's how we create it.
+        new myInterface(){
+            public void prentData() {
+                System.out.println("Welcome to anonymouse object and class");
+            }
+            public int getData() {
+                return 0;
+            }
+        }.prentData();
+
+
     }
+
+
+
+
 }
